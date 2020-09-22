94214e5d1c883ae7bc240ef935f53e8f8f11c54c
Alexsey Shishkin <shishkinalexsey42@gmail.com>
2020-09-22 18:32:59 +0300

    fix

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./src/assets/db.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 4001
+const router = jsonServer.router('../src/assets/db.json')
+const middlewares = jsonServer.defaults({
+  static: './build',
+})
+
+const PORT = process.env.PORT || 3001
+
 server.use(middlewares)
 server.use(router)
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+
+server.listen(PORT, () => {
+  console.log('Server is running')
 })
